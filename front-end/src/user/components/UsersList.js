8a80ccd6d7a4765e7f0978543037bee695483560
jackdo69 <jackie.duc.do@gmail.com>
2020-01-30 23:55:36 +1100

    side drawer initial

--- front-end/src/user/components/UsersList.js
+++ front-end/src/user/components/UsersList.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./UsersList.css";
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Avatar";
 
 const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found</h2>
+        <Card>
+          <h2>No users found</h2>
+        </Card>
       </div>
     );
   }
@@ -14,12 +17,15 @@ const UsersList = props => {
   return (
     <ul className="users-list">
       {props.items.map(user => {
-        return <UserItem
-          key={user.id}
-          id={user.id}
-          image={user.image}
-          name={user.name}
-          placeCount={user.places} />;
+        return (
+          <UserItem
+            key={user.id}
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            placeCount={user.places}
+          />
+        );
       })}
     </ul>
   );
