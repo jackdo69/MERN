3d7b8a4175ceb3796159f7a35d57eeb2f2334dc9
jackdo69 <jackie.duc.do@gmail.com>
2020-02-03 18:08:43 +1100

    finish the add place form

--- front-end/src/shared/components/FormElements/Input.js
+++ front-end/src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import "./Input.css";
 import { validate } from "../../util/validators";
 
@@ -27,6 +27,14 @@ const Input = props => {
     isValid: false,
     isTouched: false
   });
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
+
   const changeHandler = event => {
     dispatch({
       type: "CHANGE",
@@ -63,7 +71,8 @@ const Input = props => {
 
   return (
     <div
-      className={`form-control ${!inputState.isValid && inputState.isTouched &&
+      className={`form-control ${!inputState.isValid &&
+        inputState.isTouched &&
         "form-control--invalid"}`}
     >
       <label htmlFor={props.id}>{props.label}</label>
