8d2c79c00874ef2e3786a82cb0429755909e3ed8
jackdo69 <jackie.duc.do@gmail.com>
2020-02-05 18:33:56 +1100

    error handling

--- back-end/app.js
+++ back-end/app.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const placesRoutes = require('./routes/places-routes');
+const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
-app.use(placesRoutes);
+app.use("/api/places", placesRoutes); //Filter the routes
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
 
 //Ignore the favicon
-let ignoreFavicon = (req, res, next) => {
-    if (req.originalUrl === '/favicon.ico') {
-      res.status(204).json({nope: true});
-    } else {
-      next();
-    }
+const ignoreFavicon = (req, res, next) => {
+  if (req.originalUrl === "/favicon.ico") {
+    res.status(204).json({ nope: true });
+  } else {
+    next();
   }
+};
 app.use(ignoreFavicon);
 
 app.listen(5000);
--- back-end/routes/places-routes.js
+++ back-end/routes/places-routes.js
@@ -1,10 +1,52 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    console.log('GET Request in Places');
-    res.json({message: 'It works!'});
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous scky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531
+    },
+    creator: "u1"
+  }
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; //{pid: 'p1'}
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+
+  res.json({ place }); // => {place} => {place: place}
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+
+  res.json({ place });
 });
 
 module.exports = router;
