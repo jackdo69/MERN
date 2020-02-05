eedc65966362a239a3562ce3fc5f9da62fd4af40
jackdo69 <jackie.duc.do@gmail.com>
2020-02-05 18:52:32 +1100

    added controllers

---
+++ back-end/controllers/places-controllers.js
@@ -0,0 +1,44 @@
+const HttpError = require("../models/http-error");
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous scky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531
+    },
+    creator: "u1"
+  }
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid; //{pid: 'p1'}
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+  res.json({ place }); // => {place} => {place: place}
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+  res.json({ place });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- back-end/routes/places-routes.js
+++ back-end/routes/places-routes.js
@@ -1,52 +1,8 @@
 const express = require("express");
-
 const router = express.Router();
+const placesControllers = require("../controllers/places-controllers");
 
-const HttpError = require("../models/http-error");
-
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous scky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
-    address: "20 W 34th St, New York, NY 10001, United States",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878531
-    },
-    creator: "u1"
-  }
-];
-
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid; //{pid: 'p1'}
-  const place = DUMMY_PLACES.find(p => {
-    return p.id === placeId;
-  });
-
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
-  }
-
-  res.json({ place }); // => {place} => {place: place}
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-
-  const place = DUMMY_PLACES.find(p => {
-    return p.creator === userId;
-  });
-
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
-    );
-  }
-
-  res.json({ place });
-});
+router.get("/:pid", placesControllers.getPlaceById);
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
 module.exports = router;
