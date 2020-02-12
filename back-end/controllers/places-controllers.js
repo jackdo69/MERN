7df7162a4617cfb9e1d9059812e3223026dac3a4
jackdo69 <jackie.duc.do@gmail.com>
2020-02-12 20:16:06 +1100

    more security features in backend

--- back-end/controllers/places-controllers.js
+++ back-end/controllers/places-controllers.js
@@ -136,6 +136,10 @@ const updatePlace = async (req, res, next) => {
     );
     return next(error);
   }
+  if (place.creator.toString() !== req.userData.userId) {
+    const error = new HttpError("You are not allowed to edit this place", 401);
+  }
+
   place.title = title;
   place.description = description;
 
@@ -169,6 +173,10 @@ const deletePlace = async (req, res, next) => {
   if (!place) {
     const error = new HttpError("Could not find place for this id.", 404);
     return next(error);
+  }
+
+  if (place.creator.id !== req.userData.userId) {
+    const error = new HttpError("You are not allowed to delete this place", 401);
   }
 
   const imagePath = place.image;
